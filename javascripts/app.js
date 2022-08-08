593ffab4796ac271949434687bf37c6f79bafc18
dd301 <donatodoley1@gmail.com>
2022-08-08 16:02:14 +0530

    tabs 1 and 2 complete

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,16 @@
 var main = function(){
-    "use strict";
+    
+    var toDos = [
+        "Finish reading this book",
+        "Take myself for a walk",
+        "Answer emails",
+        "Prep for Monday's class",
+        "Make up some new ToDos",
+        "Get groceries"
+    ];
+    
     $(".tabs a span").toArray().forEach(function(element){
+        
         //create a click handler for this element
         $(element).on("click", function(){
             var $element = $(element);
@@ -9,17 +19,30 @@ var main = function(){
             $("main .content").empty();
 
             if ($element.parent().is(":nth-child(1)")){
-                console.log("FIRST TAB CLICKED!")
+                console.log("FIRST TAB CLICKED!");
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
             }
             else if ($element.parent().is(":nth-child(2)")){
-                console.log("SECOND TAB CLICKED!")
+                console.log("SECOND TAB CLICKED!");
+                $content = $("<ul>");
+                toDos.forEach(function (todo){
+                    $content.append($("<li>").text(todo));
+                });
             }
             else if ($element.parent().is(":nth-child(3)")){
-                console.log("THIRD TAB CLICKED!")
+                console.log("THIRD TAB CLICKED!");
             }
+            
+            $("main .content").append($content);
+            
             return false;
         });
     });
+    
+    $(".tabs a:first-child span").trigger("click");
 };
 
 $(document).ready(main);
